abf7ff9474e4851d8130581f87850be32f88b6be
Gabriela D <164107373+gabhmmph@users.noreply.github.com>
2026-05-23 16:52:22 -0300

    Add files via upload

---
+++ Atividade045/src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Alan";
+        meuAluno.idade = 18;
+
+        meuAluno.exibeInformacoes();
+
+    }
+}
